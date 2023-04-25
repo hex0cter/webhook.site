d3bf32895214ef5bc5c0c5b377a4e80817d36b3c
Daniel Han <daniel.han@scrive.com>
2023-04-25 21:22:29 +0200

    Cleanup unused parts

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,11 +35,11 @@ class Kernel extends HttpKernel
             StartSession::class,
             ShareErrorsFromSession::class,
             Middleware\VerifyCsrfToken::class,
-            'throttle:30,1',
+            // 'throttle:30,1',
         ],
 
         'api' => [
-            'throttle:30,1',
+            // 'throttle:30,1',
         ],
     ];
 
@@ -54,6 +54,6 @@ class Kernel extends HttpKernel
         'auth' => Middleware\Authenticate::class,
         'auth.basic' => AuthenticateWithBasicAuth::class,
         'guest' => Middleware\RedirectIfAuthenticated::class,
-        'throttle' => Middleware\ThrottleRequests::class,
+        // 'throttle' => Middleware\ThrottleRequests::class,
     ];
 }
